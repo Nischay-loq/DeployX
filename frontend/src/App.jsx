4309d5db21b168e3141c97162aafa193b6651337
Chetan Chaudhari <chetankc2005@gmail.com>
2025-08-05 20:14:46 +0530

    Updated Terminal Feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Remote Terminal</h1>
-      </header>
-      <main>
-        <Terminal />
-      </main>
-    </div>
+    <main>
+      <Terminal />
+    </main>
   );
 }
 
